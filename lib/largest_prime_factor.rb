c6540ffc53194a52162b8a0eb96af2e0c1ebd1cf
lovecosma <williamsmatthew333@gmail.com>
2021-06-23 21:54:26 -0600

    commit tree ab4833d1f4ff2a13df6d3660dfc0c0b130020ff0

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,31 @@
-# Enter your procedural solution here!
+require 'pry'# Enter your procedural solution here!
+def largest_prime_factor(input)
+
+    max_prime = -1
+
+    while input % 2 == 0
+
+        max_prime = 2
+    input = input / 2
+       
+    end 
+
+   counter = 3
+
+   while counter <= Math.sqrt(input)
+
+    if input % counter == 0
+        max_prime = counter
+         input = input / counter
+    end 
+
+    counter += 2
+   end 
+
+   if input > 2
+    max_prime = input
+   end 
+    
+    max_prime
+end 
+
